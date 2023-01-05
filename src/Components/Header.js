56b8c19874b131440ad15aac18740f123cf876f0
Anshul Aggarwal <aggarwalanshul1997@gmail.com>
2023-01-05 23:55:55 +0530

    initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+        <h2>Meme Generator</h2>
+    </div>
+  )
+}
---
+++ src/Components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <>
+      <div>
+        <input type="text" placeholder="Enter upper text"></input>
+        <input type="text" placeholder="Enter lower text"></input>
+        <button>Choose an image</button>
+      </div>
+    </>
+  );
+}
